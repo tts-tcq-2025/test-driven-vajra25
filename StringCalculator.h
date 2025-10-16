a943a105b8a3085123b0edbab4e3d5aada2a5b19
Vajra <VajraJagadish.Shetty@in.bosch.com>
2025-10-16 20:43:41 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -6,13 +6,7 @@
 
 class StringCalculator {
 public:
-    int Add(const std::string& numbers);
-
-private:
-    std::string extractCustomDelimiter(std::string& numbers);
-    std::vector<std::string> split(const std::string& str, const std::vector<std::string>& delimiters);
-    int computeSum(const std::vector<std::string>& tokens);
-    void checkNegatives(const std::vector<int>& negatives);
+    int Add(const std::string &numbers);
 };
 
-#endif // STRINGCALCULATOR_H
+#endif
