8c43dc1f15c934645daa99bcbd7d770f2a747047
Vajra <VajraJagadish.Shetty@in.bosch.com>
2025-10-16 20:47:44 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -7,6 +7,12 @@
 class StringCalculator {
 public:
     int Add(const std::string &numbers);
+
+private:
+    std::string getDelimiter(std::string &numbers);
+    std::vector<std::string> split(const std::string &s, const std::string &delims);
+    int toInt(const std::string &token, std::vector<int> &negatives);
+    void throwIfNegatives(const std::vector<int> &negatives);
 };
 
 #endif
