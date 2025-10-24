e79225bc39be158a15dcf84f2b891be2b9978f31
Vajra <VajraJagadish.Shetty@in.bosch.com>
2025-10-24 11:32:55 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -1,7 +1,5 @@
 #include "StringCalculator.h"
 #include <string>
-#include <vector>
-#include <sstream>
 #include <stdexcept>
 
 int StringCalculator::Add(const std::string& numbers) {
@@ -11,43 +9,43 @@ int StringCalculator::Add(const std::string& numbers) {
     std::string content = numbers;
 
     // Custom delimiter
-    if (numbers.size() >= 2 && numbers[0] == '/' && numbers[1] == '/') {
-        size_t newlinePos = numbers.find('\n');
-        delimiter = numbers.substr(2, newlinePos - 2);
-        content = numbers.substr(newlinePos + 1);
+    if (numbers.substr(0, 2) == "//") {
+        size_t pos = numbers.find('\n');
+        delimiter = numbers.substr(2, pos - 2);
+        content = numbers.substr(pos + 1);
 
-        // Remove brackets for long delimiter
+        // Remove brackets for long delimiters
         if (delimiter.size() > 2 && delimiter.front() == '[' && delimiter.back() == ']')
             delimiter = delimiter.substr(1, delimiter.size() - 2);
     }
 
     // Replace newlines with delimiter
-    for (size_t i = 0; i < content.size(); ++i) {
+    for (size_t i = 0; i < content.size(); ++i)
         if (content[i] == '\n') content[i] = delimiter[0];
-    }
 
-    // Split and convert to integers
-    std::vector<int> nums;
-    std::stringstream ss(content);
-    std::string token;
-    while (getline(ss, token, delimiter[0])) {
-        if (token.empty()) continue;
-        int val = std::stoi(token);
-        if (val <= 1000) nums.push_back(val);
-    }
-
-    // Check negatives
-    std::string negs;
-    for (size_t i = 0; i < nums.size(); ++i) {
-        if (nums[i] < 0) {
-            if (!negs.empty()) negs += ",";
-            negs += std::to_string(nums[i]);
+    // Split and process numbers
+    int sum = 0;
+    std::string temp;
+    std::string negatives;
+    for (size_t i = 0; i <= content.size(); ++i) {
+        if (i == content.size() || content[i] == delimiter[0]) {
+            if (!temp.empty()) {
+                int num = std::stoi(temp);
+                if (num < 0) {
+                    if (!negatives.empty()) negatives += ",";
+                    negatives += std::to_string(num);
+                } else if (num <= 1000) {
+                    sum += num;
+                }
+                temp.clear();
+            }
+        } else {
+            temp += content[i];
         }
     }
-    if (!negs.empty()) throw std::invalid_argument("negatives not allowed: " + negs);
 
-    // Sum
-    int sum = 0;
-    for (int n : nums) sum += n;
+    if (!negatives.empty())
+        throw std::invalid_argument("negatives not allowed: " + negatives);
+
     return sum;
 }
